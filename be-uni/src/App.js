33093c7fda4b8117bf7dc4a0227530f7be5f51ae
Bruna Nallis <bruna.gnallis@gmail.com>
2023-03-23 15:11:12 -0300

    filters done

--- be-uni/src/App.js
+++ be-uni/src/App.js
@@ -1,9 +1,44 @@
+import React, {useState} from "react";
 import Filter from "./components/Filters/Filters";
+import { produtos } from "./prodcts";
 
-function App() {
+ function App() {
+  // const state = await axios.get('https://api.beuni.com.br/atlas/brands/v2/products?q=&category=&min=0&max=99999&sortBy=featured&page=1&perPage=50')
+
+  const [maxFilter, setMaxFilter] = useState(10000);
+  const [minFilter, setMinFilter] = useState(0)
+  const [search, setSearch] = useState('')
+
+  function onChangeSearch (e) {
+    setSearch (e.target.value)
+  }
+
+  function onChangeMinPrice (e) {
+    setMinFilter (e.target.value)
+  } 
+
+  function onChangeMaxPrice (e) {
+    setMaxFilter(e.target.valeu)
+  }
+  
+  const filterMin =  produtos.filter(produto => {
+    if(maxFilter){
+      return produto.price <= maxFilter
+    }
+  }
+  )
+
+  console.log(filterMin)
+
   return (
     <div>
-      <Filter/>
+      <Filter
+      minPrice={minFilter}
+      maxPrice={maxFilter}
+      busca={search}
+      onChangeMaxPrice={onChangeMaxPrice}
+      onChangeMinPrice={onChangeMinPrice}
+      onChangeSearch={onChangeSearch}/>
     </div>
   );
 }
--- be-uni/src/components/Filters/Filters.js
+++ be-uni/src/components/Filters/Filters.js
@@ -3,14 +3,14 @@ import { GroupFilters, InputGroup } from './styled';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-export default function Filter() {
+export default function Filter(props) {
     return (
         <div>
             <h2 align="center">Filtrar Produtos</h2>
             <InputGroup>
                 <GroupFilters>
                     <label >Preço máximo</label>
-                    <TextField id="outlined-basic" type="number"
+                    <TextField type="number"
                         placeholder='R$ Máx'
                         sx={{
                             width: 500,
@@ -19,7 +19,9 @@ export default function Filter() {
                         InputLabelProps={{
                             shrink: true,
                         }}
-                        variant="outlined" />
+                        variant="outlined"
+                        value={props.maxPrice}
+                        onChange={props.onChangeMaxPrice} />
                 </GroupFilters>
                 <GroupFilters>
                     <label>Preço minimo</label>
@@ -32,22 +34,21 @@ export default function Filter() {
                         InputLabelProps={{
                             shrink: true,
                         }}
-                        variant="outlined" />
+                        variant="outlined"
+                        value={props.minPrice}
+                        onChange={props.onChangeMinPrice} />
                 </GroupFilters>
                 <GroupFilters>
                     <label>Buscar por produto</label>
-                    <Autocomplete
-                        disablePortal
-                        id="combo-box-demo"
-                        sx={{
-                            width: 500,
-                            maxWidth: '100%',
-                        }}
-                        renderInput={(params) => <TextField {...params}  placeholder='Buscar' />}
-                    />
-                    {/* options={top100Films} */}
+                     <TextField
+                     
+                            onChange={props.onChangeBusca}
+                            placeholder='Buscar'
+                            value={props.busca}
+                        />
                 </GroupFilters>
             </InputGroup>
+
         </div>
     )
 
